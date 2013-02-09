ab95358b85cdbfc82231bf5c896e80974a61d75e
Edward Murrell <edward@murrell.co.nz>
2013-02-09 12:43:21 +1100

    Initial options header for class based options handling

---
+++ src/mogvOptions.h
@@ -0,0 +1,13 @@
+#define MOG_OPT_TYPE_UINT 1
+#define MOG_OPT_TYPE_SINT 3
+#define MOG_OPT_TYPE_CHAR 4
+#define MOG_OPT_TYPE_UINT_ARRAY 129
+#define MOG_OPT_TYPE_SINT_ARRAY 131
+#define MOG_OPT_TYPE_CHAR_ARRAY 132
+
+namespace mogvert
+{
+  class mogvOptions
+  {
+  };
+}
