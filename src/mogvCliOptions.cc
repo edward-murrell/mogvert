3ce53864a8ffe96171fcc6418d9bab903ce0cbef
Edward Murrell <edward@murrell.co.nz>
2013-02-09 14:28:08 +1100

    Add empty cli implementation

---
+++ src/mogvCliOptions.cc
@@ -0,0 +1,8 @@
+#include "mogvCliOptions.h"
+using namespace mogvert;
+
+mogvCliOptions::mogvCliOptions(int argc,  char* argv[]) {
+}
+mogv_options mogvCliOptions::getOptions(mogv_opt_type option_type) {
+}
+
