3185f1e1e2d2448ddb4c83dfab7f6c6bdf5fc990
Edward Murrell <edward@murrell.co.nz>
2013-04-07 17:42:57 +1000

    Added AO & mp3 encoders to registry

--- src/mogvModuleRegistery.cc
+++ src/mogvModuleRegistery.cc
@@ -1,9 +1,13 @@
 #include "mogvModuleRegistery.h"
 #include "decoders/ogg.h"
 #include "decoders/flac.h"
+#include "encoders/mp3.h"
+#include "encoders/ao.h"
 
 void mogv_register_modules() {
 	mogvModuleRegister * reg = mogvModuleRegister::getStaticInstance();
 	reg->registerModule(new ogg_decoder_proxy);
 	reg->registerModule(new flac_decoder_proxy);
+	reg->registerModule(new mp3_encoder_proxy);
+	reg->registerModule(new ao_encoder_proxy);	
 }
