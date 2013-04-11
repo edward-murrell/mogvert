20321c58c286a8fc3eb65b8bd78efa5b48774f9b
Edward Murrell <edward@murrell.co.nz>
2013-04-11 23:28:56 +1000

    Add compile instructions to the testProxyRegister file

--- src/tests/testProxyRegister.cc
+++ src/tests/testProxyRegister.cc
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <assert.h>
 
+/* Run from the src directory
+ * g++ -Wall  -std=c++0x  -DHAVE_INTTYPES_H -o tests/testProxyRegister tests/testProxyRegister.cc mogvModuleProxy.cc mogvModuleRegistery.cc mogvModuleRegister.cc decoders/ogg.cc decoders/flac.cc encoders/mp3.cc encoders/ao.cc -lmp3lame -logg -lvorbisfile -lvorbis -lao -lFLAC -lFLAC++
+ */
+
 int main (int arc, char* argv[]) { 
 	mogv_register_modules();
 	mogvModuleRegister * reg = mogvModuleRegister::getStaticInstance();
