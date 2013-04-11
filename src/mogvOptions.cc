bf07abbe5f59b59330cb920256389fb6cb8f535e
Edward Murrell <edward@murrell.co.nz>
2013-04-11 23:34:30 +1000

    Add the basic test and stub for mogvOptions

---
+++ src/mogvOptions.cc
@@ -0,0 +1,15 @@
+#include "mogvOptions.h"
+
+using namespace mogvert;
+
+void OptionsHandler::registerOption(
+    const char * name,
+    const char * description,
+    mogv_opt_type type,
+    void (OptionsHandler::*method)(mogv_option * opt)) {
+    
+}
+void OptionsHandler::setOption(mogv_option * opt) {
+    
+}
+void OptionsHandler::setOptionStack(mogv_options * opts) {}
---
+++ src/tests/testOptions.cc
@@ -0,0 +1,11 @@
+#include "../mogvOptions.h"
+#include <iostream>
+#include <assert.h>
+
+/* Run from the src directory
+ * g++ -Wall  -std=c++0x  -DHAVE_INTTYPES_H -o  tests/testOptions tests/testOptions.cc 
+ */
+
+int main (int arc, char* argv[]) { 
+	exit(0);
+}
