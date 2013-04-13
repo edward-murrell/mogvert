382b809cc52213aa37ae557e2317ddba35a56ac4
Edward Murrell <edward@murrell.co.nz>
2013-04-13 16:43:41 +1000

    Remove utterly irrelevant comments

--- src/mogvModuleProxy.cc
+++ src/mogvModuleProxy.cc
@@ -11,13 +11,3 @@ void mogvModuleProxy::whoAmI() { // TODO Remove this in favour of a test module 
 coder_info * mogvModuleProxy::getModuleInfo() {
 	return this->info;
 }
-
-// Really, what we want is this;
-// With some magic where by it pulls the ogg_decoders static info from... somewhere
-// MogvModuleRegister::getInstance()->registerModule(mogvModuleProxy<ogg_decoder>);
-//
-// Since, it's not possible to put have static virtual methods required for getting
-// the en/decoder information, it may as well go in it's own (non static) virtual
-// method, and force the create[en|dec]coder methods to be overriden.
-//
-// MogvModuleRegister::getInstance()->registerModule(new ogg_decoder_proxy);
